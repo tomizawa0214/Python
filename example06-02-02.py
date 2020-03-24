078d9adedde25decd5222c93d1e7261222e3ef06
tomizawa0214 <j.tomizawa.japan@gmail.com>
2020-03-24 16:43:46 +0900

    Python Practice⑤

---
+++ example06-02-02.py
@@ -0,0 +1,6 @@
+# coding:utf-8
+import tkinter as tk
+
+root = tk.Tk()
+root.geometry("400x150")
+root.mainloop()
---
+++ example06-02-03.py
@@ -0,0 +1,7 @@
+# coding:utf-8
+import tkinter as tk
+
+root = tk.Tk()
+root.geometry("400x150")
+root.title("数当てゲーム")
+root.mainloop()
