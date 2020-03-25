c988117712f5d11f98e2867749e2b94c2ece1095
tomizawa0214 <j.tomizawa.japan@gmail.com>
2020-03-25 18:12:28 +0900

    Python Practice⑮

--- challenge.py
+++ challenge.py
@@ -1,18 +1,22 @@
 # coding:utf-8
 
 class User:
-    count = 0
     def __init__(self, name):
-        User.count += 1 
         self.name = name
-
     def say_hi(self):
         print("hi {0}".format(self.name))
-    @classmethod
-    def show_info(cls):
-        print("{0} instances" .format(cls.count))
 
-tom = User("tom")
-bob = User("bob")
+class AdminUser(User):
+    def __init__(self, name, age):
+        super().__init__(name)
+        self.age = age
+    def say_hello(self):
+        print("hello {0} ({1})".format(self.name, self.age))
+    def say_hi(self):
+        print("[admin] hi {0}".format(self.name))
 
-User.show_info()
+bob = AdminUser("bob", 23)
+print(bob.name)
+bob.say_hi()
+bob.say_hello()
+
