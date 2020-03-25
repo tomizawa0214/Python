dc56525da35f29b8bd7b90cc3c57a9ebf0192951
tomizawa0214 <j.tomizawa.japan@gmail.com>
2020-03-25 17:30:11 +0900

    Python Pravtice⑩

--- challenge.py
+++ challenge.py
@@ -1,9 +1,12 @@
 # coding:utf-8
 
-def say_hi(name, age = 20):
-    print("hi{0} ({1})".format(name, age))
 
-say_hi("tomi", 23)
-say_hi("bob", 21)
-say_hi("steve")
-say_hi(age = 18, name = "rick")
+msg = "hello"
+
+def say_hi():
+    global msg
+    msg = "hello global"
+    print(msg)
+    
+say_hi()
+print(msg)
