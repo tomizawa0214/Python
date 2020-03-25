9d22d390ae64a38277267bfe22e6795a7b5c5b7a
tomizawa0214 <j.tomizawa.japan@gmail.com>
2020-03-25 17:21:01 +0900

    Python Pravtice⑨

--- challenge.py
+++ challenge.py
@@ -1,8 +1,9 @@
 # coding:utf-8
 
-for i in range(10):
-    if i == 5:
-        continue
-    print(i)
-else:
-    print("end")
+def say_hi(name, age = 20):
+    print("hi{0} ({1})".format(name, age))
+
+say_hi("tomi", 23)
+say_hi("bob", 21)
+say_hi("steve")
+say_hi(age = 18, name = "rick")
