d4f571a4c056408d08245ab39b9fe79f14ed8591
tomizawa0214 <j.tomizawa.japan@gmail.com>
2020-03-26 09:33:31 +0900

    Python Practice㉔

--- challenge.py
+++ challenge.py
@@ -1,22 +1,3 @@
 # coding:utf-8
 
-scores = [40, 50, 70, 90, 60]
-it = iter(scores)
-print(next(it))
-print(next(it))
-print("hello")
-print(next(it))
-
-for score in scores:
-    print(score)
-
-def get_infinite():
-    i =0
-    while True:
-        yield i * 2
-        i += 1
-
-g = get_infinite()
-print(next(g))
-print(next(g))
-print(next(g))
+print(list(map(lambda n: n * 3, [1, 2, 3])))
