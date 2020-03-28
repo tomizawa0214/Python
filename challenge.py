49775cb3379c34e3d498dd167e547f48edf2b473
tomizawa0214 <j.tomizawa.japan@gmail.com>
2020-03-28 22:16:24 +0900

    関数の引数

--- challenge.py
+++ challenge.py
@@ -84,3 +84,63 @@
 # result = get_ans4ultimateQ()
 # print(get_ans4ultimateQ())
 # print(result)
+
+# def myfunc(param1, param2, param3):
+#     return f'param1: {param1}, param2: {param2}, param3: {param3}'
+
+# print(myfunc(param3=1, param2='string', param1=0.5))
+# print(myfunc(*'abc'))
+# print(myfunc(1, *'23'))
+# print(myfunc(*'12', 3))
+# arg_dict = {'param3': 'param3', 'param2': 2, 'param1': 1.0}
+# print(myfunc(**arg_dict))
+
+# def myfunc(param1='param1', param2='parm2', param3='param3'):
+#     return f'param1: {param1}, param2: {param2}, param3: {param3}'
+
+# print(myfunc())
+# print(myfunc(1))
+# print(myfunc(param2=2))
+
+# def myfunc(param1='param1', param2, param3):
+#     return f'param1: {param1}, param2: {param2}, param3: {param3}'
+
+# print(myfunc(param2=2, param3=3))
+
+# def myfunc(param1, param2, *args):
+#     return f'param1: {param1}, param2: {param2}, other: {args}'
+
+# print(myfunc(1, 2))
+# print(myfunc(1, 2, 3))
+# print(myfunc(1, 2, 3, 4))
+
+# def myfunc(param1, param2, *args):
+#     tmp = f'param1: {param1}, param2: {param2}'
+#     index = 3
+#     for item in args:
+#         tmp += f', param{index}: {item}'
+#         index += 1
+#     return tmp
+
+# print(myfunc(1, 2))
+# print(myfunc(1, 2, 3))
+# print(myfunc(1, 2, 3, 4))
+# print(myfunc(1, 2, 2, 3, 4))
+
+# def myfunc(*args, param1):
+#     return f'param1: {param1}, others: {args}'
+
+# print(myfunc(1, param1='2'))
+
+# def myfunc(param1, **kwargs):
+#     return f'param1: {param1}, others: {kwargs}'
+
+# print(myfunc(param4='param2', param1=1, fool='foo'))
+
+def myfunc(param1, **kwargs):
+    tmp = f'param1: {param1}'
+    for item in kwargs.items():
+        tmp += f', {item[0]}: {item[1]}'
+    return tmp
+
+print(myfunc(param2='param2', param1=1, foo='foo'))
