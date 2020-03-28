7c1d0c870bdb37e6aa5402d61ceea53081be0cf1
tomizawa0214 <j.tomizawa.japan@gmail.com>
2020-03-28 17:10:28 +0900

    Pythonのfor文練習

--- challenge.py
+++ challenge.py
@@ -13,14 +13,60 @@
 # if number % 2 == 0:
 #     print('even')
 
-number = input('何か値を入力してください:')
-number = int(number)
+# number = input('何か値を入力してください:')
+# number = int(number)
 
-if number % 15 == 0:
-    print('FizzBuzz')
-elif number % 3 == 0:
-    print('Fizz')
-elif number % 5 == 0:
-    print('Buzz')
-else:
-    print(number)
+# if number % 15 == 0:
+#     print('FizzBuzz')
+# elif number % 3 == 0:
+#     print('Fizz')
+# elif number % 5 == 0:
+#     print('Buzz')
+# else:
+#     print(number)
+
+# names = ['一色', 'かわさき', '遠藤']
+# target = 'かわさき'
+
+# for name in names:
+#     if target in name:
+#         print(f'発見: {name}')
+#         break
+# else:
+#     print('見つかりませんでした')
+
+# names = ['一色', 'かわさき', 'かわさきしんじ', '遠藤']
+# target = 'かわさき'
+
+# for name in names:
+#     if target in name:
+#         print(f'発見: {name}')
+#         break
+#     print('繰り返し処理を継続します')
+# else:
+#     print('見つかりませんでした')
+
+# names = ['一色', 'かわさき', 'かわさきしんじ', '遠藤']
+# target = 'かわさき'
+
+# for name in names:
+#     if target in name:
+#         print(f'発見: {name}')
+#         continue
+#     print('繰り返し処理を継続します')
+# else:
+#     print('見つかりませんでした')
+
+names = ['一色', 'かわさき', 'かわさきしんじ', '遠藤']
+target = 'かわさき'
+found = False
+
+for name in names:
+    if target in name:
+        found = True
+        print(f'発見: {name}')
+        continue
+    print('繰り返し処理を継続します')
+    
+    if not found:
+        print('見つかりませんでした')
