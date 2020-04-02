ab812a9887951627a455d6cdb346fddbd0f1a8e5
tomizawa0214 <j.tomizawa.japan@gmail.com>
2020-04-02 09:17:43 +0900

    Python入門-例外処理

--- exception.py
+++ exception.py
@@ -16,38 +16,78 @@
 
 # print('素晴らしい！正解です！')
 
-while True:
-    try:
-        print()
-        print('1: ValueError例外を発生')
-        print('2: IndexError例外を発生')
-        print('3: 例外を発生させない')
-        print('4: 終了')
-        selection = int(input('どれにしますか:'))
-        if selection == 1:
-            tmp = int('foo')
-        elif selection == 2:
-            tmp = 'str'[5]
-        elif selection == 3:
-            print()
-            print('例外を発生させませんでした')
-        elif selection == 4:
-            print()
-            print('終了します')
-            break
-        else:
-            print(undefined_var)
-    except ValueError as e:
-        print('Value Error')
-        print(e.args)
-        print()
-    except IndexError:
-        print('Index Error')
-        print()
-    except Exception as e:
-        print('Exception')
-        print(e.args)
-        print()
-    print('try文の直後の行を実行しました')
+# while True:
+#     try:
+#         print()
+#         print('1: ValueError例外を発生')
+#         print('2: IndexError例外を発生')
+#         print('3: 例外を発生させない')
+#         print('4: 終了')
+#         selection = int(input('どれにしますか:'))
+#         if selection == 1:
+#             tmp = int('foo')
+#         elif selection == 2:
+#             tmp = 'str'[5]
+#         elif selection == 3:
+#             print()
+#             print('例外を発生させませんでした')
+#         elif selection == 4:
+#             print()
+#             print('終了します')
+#             break
+#         else:
+#             print(undefined_var)
+#     except Exception as e:
+#         print('Exception')
+#         print(e.args)
+#         print()
+#     except ValueError as e:
+#         print('Value Error')
+#         print(e.args)
+#         print()
+#     except IndexError:
+#         print('Index Error')
+#         print()
+#     print('try文の直後の行を実行しました')
 
-print('無限ループを終了しました')
+# print('無限ループを終了しました')
+
+# while True:
+#     try:
+#         print()
+#         print('1: 例外を発生させる')
+#         print('2: 例外を発生させない')
+#         print('3: 終了')
+#         selection = int(input('どれにしますか: '))
+#         if selection == 1:
+#             tmp = 'str'[5]
+#         elif selection == 2:
+#             print('例外を発生しませんでした')
+#         elif selection == 3:
+#             break
+#     except IndexError:
+#         print('IndexError例外が発生しました')
+#     else:
+#         print('else節を実行しました')
+#     finally:
+#         print('finally節を実行しました')
+
+def raise_exception():
+    try:
+        raise ValueError()
+    except IndexError:
+        print('Index Error caught')
+    finally:
+        print('executing finally clause')
+
+def call_raise_exception():
+    try:
+        raise_exception()
+    except ValueError:
+        print('Value Error caught')
+
+def call_raise_exception2():
+    raise_exception()
+
+call_raise_exception()
+call_raise_exception2()
